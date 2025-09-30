d39cb631b902b3670783a63a94bd76ae528f70b7
Debabrot Bhuyan <debabrot91@gmail.com>
2025-09-30 03:44:26 +0000

    Added delete, successor, predecessor functions

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -65,3 +65,86 @@ for v in values:
 print("In-order traversal:", bst.inorder())  # Sorted order
 print("Search 40:", bst.search(40))  # True
 print("Search 25:", bst.search(25))  # False
+
+
+def delete(root, key):
+    if root is None:
+        return None
+
+    # First, find the node to delete
+    if key < root.data:
+        root.left = delete(root.left, key)
+    elif key > root.data:
+        root.right = delete(root.right, key)
+    else:  # Found the node to delete
+
+        # Case 1: No children
+        if root.left is None and root.right is None:
+            return None
+        # Case 2: One child
+        elif root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+        # Case 3: Two children
+        else:
+            # Find inorder successor (smallest in right subtree)
+            successor = find_min(root.right)
+            root.data = successor.data
+            root.right = delete(root.right, successor.data)
+    
+    return root
+
+def find_min(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+def successor(root, node):
+    if root is None or node is None:
+        return None
+    
+    # Case 1: Node has right subtree
+    if node.right:
+        current = node.right
+        while current.left:
+            current = current.left
+        return current
+    
+    # Case 2: No right subtree - find ancestor
+    successor_node = None
+    current = root
+    while current:
+        if current.data > node.data:
+            successor_node = current
+            current = current.left
+        elif current.data < node.data:
+            current = current.right
+        else:
+            break
+    return successor_node
+
+def predecessor(root, node):
+    if root is None or node is None:
+        return None
+    
+    # Case 1: Node has left subtree
+    if node.left:
+        current = node.left
+        while current.right:
+            current = current.right
+        return current
+    
+    # Case 2: No left subtree - find ancestor
+    predecessor_node = None
+    current = root
+    while current:
+        if current.data < node.data:
+            predecessor_node = current
+            current = current.right
+        elif current.data > node.data:
+            current = current.left
+        else:
+            break
+    return predecessor_node
