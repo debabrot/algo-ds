310696316507c152a145c8691708790f8cf12179
Debabrot Bhuyan <debabrot91@gmail.com>
2025-11-13 06:32:02 +0000

    Updated functions

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -1,150 +1,128 @@
+from typing import Optional
+
+
 class TreeNode:
-    def __init__(self, value):
+    def __init__(self, value: int) -> None:
         self.value = value
-        self.left = None
-        self.right = None
+        self.left: Optional[TreeNode] = None
+        self.right: Optional[TreeNode] = None
 
 
-class BST:
-    def __init__(self):
-        self.root = None
-
-    # Insert a value into the BST
-    def insert(self, value):
-        if self.root is None:
-            self.root = TreeNode(value)
-        else:
-            self._insert_recursive(self.root, value)
-
-    def _insert_recursive(self, node, value):
-        if value < node.value:
-            if node.left is None:
-                node.left = TreeNode(value)
-            else:
-                self._insert_recursive(node.left, value)
-        elif value > node.value:
-            if node.right is None:
-                node.right = TreeNode(value)
-            else:
-                self._insert_recursive(node.right, value)
-
-    # Search for a value in the BST
-    def search(self, value):
-        return self._search_recursive(self.root, value)
-
-    def _search_recursive(self, node, value):
-        if node is None:
-            return False
-        if node.value == value:
-            return True
-        elif value < node.value:
-            return self._search_recursive(node.left, value)
-        else:
-            return self._search_recursive(node.right, value)
-
-    # In-order traversal (Left -> Root -> Right)
-    def inorder(self):
-        result = []
-        self._inorder_recursive(self.root, result)
-        return result
-
-    def _inorder_recursive(self, node, result):
-        if node:
-            self._inorder_recursive(node.left, result)
-            result.append(node.value)
-            self._inorder_recursive(node.right, result)
-
-
-# Example usage
-bst = BST()
-values = [50, 30, 70, 20, 40, 60, 80]
-
-for v in values:
-    bst.insert(v)
-
-print("In-order traversal:", bst.inorder())  # Sorted order
-print("Search 40:", bst.search(40))  # True
-print("Search 25:", bst.search(25))  # False
-
-
-def delete(root, key):
+def insert(root: Optional[TreeNode], value: int) -> TreeNode:
     if root is None:
-        return None
-
-    # First, find the node to delete
-    if key < root.data:
-        root.left = delete(root.left, key)
-    elif key > root.data:
-        root.right = delete(root.right, key)
-    else:  # Found the node to delete
-
-        # Case 1: No children
-        if root.left is None and root.right is None:
-            return None
-        # Case 2: One child
-        elif root.left is None:
-            return root.right
-        elif root.right is None:
-            return root.left
-        # Case 3: Two children
+        return TreeNode(value)
+    else:
+        if root.value > value:
+            root.left = insert(root.left, value)
         else:
-            # Find inorder successor (smallest in right subtree)
-            successor = find_min(root.right)
-            root.data = successor.data
-            root.right = delete(root.right, successor.data)
-    
+            root.right = insert(root.right, value)
     return root
 
-def find_min(node):
-    current = node
+
+def search(root: Optional[TreeNode], value: int) -> Optional[TreeNode]:
+    if root is None or root.value == value:
+        return root
+    if root.value > value:
+        return search(root.left, value)
+    return search(root.right, value)
+
+
+
+def print_preorder(root: Optional[TreeNode]) -> None:
+    if root is None:
+        return None
+    print(root.value)
+    print_preorder(root.left)
+    print_preorder(root.right)
+
+
+def print_inorder(root: Optional[TreeNode]) -> None:
+    if root is None:
+        return None
+    print_inorder(root.left)
+    print(root.value)
+    print_inorder(root.right)
+
+
+def print_postorder(root: Optional[TreeNode]) -> None:
+    if root is None:
+        return None
+    print_postorder(root.left)
+    print_postorder(root.right)
+    print(root.value)
+
+
+def find_min(root: TreeNode) -> TreeNode:
+    current = root
     while current.left:
         current = current.left
     return current
 
-def successor(root, node):
-    if root is None or node is None:
-        return None
-    
-    # Case 1: Node has right subtree
-    if node.right:
-        current = node.right
-        while current.left:
-            current = current.left
-        return current
-    
-    # Case 2: No right subtree - find ancestor
-    successor_node = None
-    current = root
-    while current:
-        if current.data > node.data:
-            successor_node = current
-            current = current.left
-        elif current.data < node.data:
-            current = current.right
-        else:
-            break
-    return successor_node
 
-def predecessor(root, node):
-    if root is None or node is None:
-        return None
-    
-    # Case 1: Node has left subtree
-    if node.left:
-        current = node.left
-        while current.right:
-            current = current.right
-        return current
-    
-    # Case 2: No left subtree - find ancestor
-    predecessor_node = None
+def find_max(root: TreeNode) -> TreeNode:
     current = root
-    while current:
-        if current.data < node.data:
-            predecessor_node = current
-            current = current.right
-        elif current.data > node.data:
-            current = current.left
+    while current.right:
+        current = current.right
+    return current
+
+
+def successor(root: Optional[TreeNode], node: TreeNode) -> Optional[TreeNode]:
+    if root is None:
+        return None
+
+    if node.right:
+        return find_min(node.right)
+    else:
+        successor_node = None
+        current = root
+        while current:
+            if current.value > node.value:
+                successor_node = current
+                current = current.left
+            elif current.value < node.value:
+                current = current.right
+            else:
+                break
+        return successor_node
+
+
+def predecessor(root: Optional[TreeNode], node: TreeNode) -> Optional[TreeNode]:
+    if root is None:
+        return None
+
+    if node.left:
+        return find_max(node.left)
+    else:
+        predecessor_node = None
+        current = root
+        while current:
+            if current.value < node.value:
+                predecessor_node = current
+                current = current.right
+            elif current.value > node.value:
+                current = current.left
+            else:
+                break
+        return predecessor_node
+
+
+def delete(root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+    if root is None:
+        return None
+    if root.value > key:
+        root = delete(root.left, key)
+    elif root.value < key:
+        root = delete(root.right, key)
+    else:
+        if root.left is None and root.right is None:
+            return None
+        elif root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
         else:
-            break
-    return predecessor_node
+            successor = find_min(root.right)
+            root.value = successor.value
+            root.right = delete(root.right, successor.value)
+        return root
+    return root
